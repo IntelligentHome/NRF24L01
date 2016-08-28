5985c0ccbfbfc520efd68383e754204afb530d93
Mariusz Sikora <msikora87@gmail.com>
2016-08-28 19:14:42 +0200

    Fix building problem.

--- Nrf24l01Avr.cc
+++ Nrf24l01Avr.cc
@@ -2,6 +2,12 @@
 
 namespace nrf24l01 {
 
+NrfStatusRegister Nrf24l01Avr::get_register_status(void) {
+    NrfStatusRegister a;
+
+    return a;
+}
+
 Status Nrf24l01Avr::read(uint8_t payload_tab[], uint8_t payload_size) {
     return STATUS_SUCCESS;
 }
--- Nrf24l01Avr.h
+++ Nrf24l01Avr.h
@@ -8,6 +8,8 @@ namespace nrf24l01 {
 class Nrf24l01Avr : public INrf24l01
 {
 public:
+    virtual NrfStatusRegister get_register_status(void);
+
     virtual Status read(uint8_t payload_tab[], uint8_t payload_size);
 
     virtual Status write(uint8_t payload_tab[], uint8_t payload_size);
